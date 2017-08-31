1f80117de10c7ebe66739e3a61f67ca356b63665
wentianen <wentianen@xingshulin.com>
2017-08-31 17:35:28 +0800

    【恩】pod init

---
+++ Example/Pods/Target Support Files/WTCarouselFlowLayout/WTCarouselFlowLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WTCarouselFlowLayout : NSObject
+@end
+@implementation PodsDummy_WTCarouselFlowLayout
+@end
--- Example/WTCarouselFlowLayout/ViewController.swift
+++ Example/WTCarouselFlowLayout/ViewController.swift
@@ -9,16 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
