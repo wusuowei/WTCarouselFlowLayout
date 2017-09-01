9872d4ab5d4cae2f10c4010f07a254e0458c6583
wentianen <wentianen@xingshulin.com>
2017-09-01 16:31:36 +0800

    carthage

---
+++ Example/WTCarouselFlowLayout/WTCarouselFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  WTCarouselFlowLayout.h
+//  WTCarouselFlowLayout
+//
+//  Created by 温天恩 on 2017/9/1.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WTCarouselFlowLayout.
+FOUNDATION_EXPORT double WTCarouselFlowLayoutVersionNumber;
+
+//! Project version string for WTCarouselFlowLayout.
+FOUNDATION_EXPORT const unsigned char WTCarouselFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WTCarouselFlowLayout/PublicHeader.h>
+
+
